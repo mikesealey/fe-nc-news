416baaac3539765488987caa8d9591c9ff0277da
Mike <mikesealey@hotmail.com>
2023-07-17 15:33:56 +0100

    completes t4 - see list of articles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,17 +8,15 @@ import Search from './Search'
 const Header = () => {
     console.log("In the header")
     return (
-        <>
+        <div id="header">
             <h1>NC News</h1>
             <nav>
-                <ul>
-                    <li> <Link to="./">Home</Link></li>
-                    <li><Link to="./topics">Topics</Link></li>
-                    <li><Link to="./users">Users</Link></li>
-                    <li><Link to="./search" >Search</Link></li>
-                </ul>
+                    <div><Link to="./">Home</Link></div>
+                    <div><Link to="./topics">Topics</Link></div>
+                    <div><Link to="./users">Users</Link></div>
+                    <div><Link to="./search" >Search</Link></div>
             </nav>
-        </>
+        </div>
     )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { Link } from "react-router-dom"
 
 const Home = () => {
-    return <h1>HOME COMPONENT</h1>
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        axios.get("https://nc-news-x8sg.onrender.com/api/articles", {})
+        .then(({data}) => {
+            console.log(data)
+            setArticles(data)
+        })
+    }, [])
+
+    return (
+        <>
+        <div className="body">
+            {articles.map((article) => {
+                return (
+                        <Link to={`/${article.article_id}`}>
+                            <div class="article-card" key={article.article_id}>
+                                <div class="headline"><h2>{article.title} by {article.author}</h2></div>
+                                <div class="details">
+                                    <div>{article.created_at}</div>
+                                    <div>{article.topic}</div>
+                                    <div>{article.votes} votes </div>
+                                    <div>{article.comment_count} comments</div>
+                                </div>
+                                <img class="thumbnail" src={article.article_img_url}/>    
+                            </div>
+                            
+                        </Link>
+                )
+            })}
+        </div>
+        </>
+    )
 }
 
 export default Home
